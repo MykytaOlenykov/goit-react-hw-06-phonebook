1583f4cd4903da9ce4042bc685df3cbaa01c1577
ForestDr <nikita2001395@gmail.com>
2023-04-07 22:04:00 +0300

    fix regExp

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -17,7 +17,7 @@ const FormError = ({ name }) => {
 const validatePattern = {
   name: /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
   number:
-    /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
+    /^\+?\d{1,4}[-.\s]?\(?\d{1,3}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/,
 };
 
 const errorMessage = {
