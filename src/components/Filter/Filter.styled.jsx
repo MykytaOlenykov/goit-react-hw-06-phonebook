916bc54b9891afa3a710033b2a79aec89152b67c
ForestDr <nikita2001395@gmail.com>
2023-04-09 21:29:28 +0300

    add fix style

--- src/components/Filter/Filter.styled.jsx
+++ src/components/Filter/Filter.styled.jsx
@@ -6,12 +6,13 @@ export const Input = styled.input`
   margin-top: 8px;
   margin-bottom: 20px;
   width: 100%;
-  border: 1px solid ${({ theme }) => theme.colors.primaryBg};
+  border: 2px solid ${({ theme }) => theme.colors.primaryBg};
   border-radius: 4px;
   box-shadow: inset 0px 1px 3px rgba(0, 0, 0, 0.12),
     0px 1px 1px rgba(0, 0, 0, 0.14);
+  outline: none;
 
   :focus {
-    outline: 2px solid ${({ theme }) => theme.colors.secondaryBg};
+    border-color: ${({ theme }) => theme.colors.secondaryBg};
   }
 `;
